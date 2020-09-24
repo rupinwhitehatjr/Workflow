4df9f0abed5f60331bf95da5fd8f2d3992af0829
Rupin <rupin@whitehatjr.com>
2020-09-24 19:15:17 +0530

    CW showing fields

--- js/viewFlow.js
+++ js/viewFlow.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
 params=getParams(window.location.href)
+//console.log(params)
 flow_id=params.id
-flowDocument=db.collection("Workflows")
+logDocument=db.collection("Workflows")
 				.doc(flow_id)
 				.collection("log")
 				.orderBy("timestamp", "desc")
 				.get()
 
-flowDocument.then(function(querySnapshot) {
+logDocument.then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
         log=doc.data();
@@ -26,6 +27,33 @@ flowDocument.then(function(querySnapshot) {
     });
 });
 
+stepsDocument=db.collection("Workflows")
+                .doc(flow_id)
+                .collection("steps")
+                .orderBy("index")
+                .get()
+
+stepsDocument.then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        step=doc.data()
+        //console.log(step)
+        stepName=step["name"]
+        //create a Dynamic Div for every Step
+        stepholder=$("<div/>")
+                    .attr("class", 'step container_12')
+                    .attr("id", doc.id)
+        //Insert the div before the comment section
+        $(stepholder).insertBefore("#commentssection")
+        stepNameDiv=$("<div/>").attr("class", "grid_12 stepheader").text(stepName)
+        $(stepholder).append($(stepNameDiv))
+        fieldsList=step["fields"]
+        for (i=0;i<fieldsList.length;i++)
+        {
+            createField(doc.id, fieldsList[i])
+        }
+    });
+});
+
 
 
 
@@ -40,4 +68,47 @@ function appendLogHTML(logText)
 	$("#actionlist").append($('<div/>').attr("class", "grid_10 logtext").append(logText))
 	$("#actionlist").append($('<div/>').attr("class", "grid_1"))
 	
+}
+
+function createField(elementid, fieldMeta)
+{
+    type=fieldMeta["type"]
+    label=fieldMeta["label"]
+    $("#"+elementid).append(
+                $('<div/>')
+                .attr("class", "grid_4 fieldlabel")
+                .append(label)
+            )
+    if(type=="dropdown")
+    {
+        
+        div=$('<div/>').attr("class", "grid_6 field")
+
+        options=fieldMeta["options"];
+        
+        dd=$("<select/>")
+        for(index=0;index<options.length;index++)
+        {
+            $(dd).append($("<option/>").val(options[index]).text(options[index]))
+        }
+        $("#"+elementid).append($(div).append($(dd)))
+
+        //Create a div to go to next line.
+        
+
+    }
+    if(type=="text")
+    {
+        
+        div=$('<div/>').attr("class", "grid_6 field")
+
+        inputBox=$('<input/>').attr("type", "text").attr('class', 'inputbox')
+        
+        $("#"+elementid).append($(div).append($(inputBox)))
+
+        //Create a div to go to next line.
+       
+
+    }
+     $("#"+elementid).append($('<div/>').attr("class", "clear"))
 }
