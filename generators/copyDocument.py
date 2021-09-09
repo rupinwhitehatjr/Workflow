518b2d90f979de17fa9b9c8cc829f8ab30d718ae
unknown <rupin@whitehatjr.com>
2021-09-09 16:59:17 +0530

    Dropdown Addition for Brazil

---
+++ generators/copyDocument.py
@@ -0,0 +1,26 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('config.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+sourceProcessName="ProcessList"
+sourceStepID="SuHYBtgcYNfLhVFrxmll"
+
+
+sourceDocs = db.collection(sourceProcessName).stream()
+
+
+for sourceDoc in sourceDocs:
+	sourceDocID=sourceDoc.id
+	if(sourceDocID==sourceStepID):
+		try:
+			a=db.collection(sourceProcessName).document().set(sourceDoc.to_dict())		
+			#print(flowID)
+		except Exception as e:
+			print(e)
+			continue	
